b64ec76ca9673d1f72ecc769124c898f8966df42
Douglas Russell <douglas.russell7@btinternet.com>
2021-12-13 15:35:56 +0000

    adds Promise.all()

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const Flight = ({updateFormData}) => {
 
     const [destinationAirport, setDestinationAirport] = useState ("");
-    const [numberOfFlights, setNumberofFlights] = useState();
+    const [numberOfFlights, setNumberOfFlights] = useState({});
     // europe berlin =          ber
     // africa SA =              tzn
     // australia/NZ sydney =    syd
@@ -20,36 +20,62 @@ const Flight = ({updateFormData}) => {
     const onChange = (e) => {
         const newDisplay = {...display};
         newDisplay[e.target.id] = !newDisplay[e.target.id]
+        if (newDisplay[e.target.id] === false) {
+            delete numberOfFlights[e.target.id]
+        }
         setDisplay(newDisplay);
+    }
+
+    const onNumFlightChange = (e) => {
+        const newObj = {...numberOfFlights};
+        newObj[e.target.name] = e.target.value;
+        setNumberOfFlights(newObj);
     }
 
     const handleFlightClick = (e) => {
         e.preventDefault();
+        let newArray = []
+        
         // if europe === true
         // getFlightEstimate{"ber"} 
         // get carbon from getFlightEstimate
         // carbon *{number_of_flights_to_europe}
 
-        let total = 0;
+      
         if (display.europe) {
-            getFlightEstimate("ber")
-
-            .then(res => {
-                console.log(res)
-                total += res.data.attributes.carbon_mt;
+            newArray.push(getFlightEstimate("ber")) 
+        }
+        if (display.africa) {
+            newArray.push(getFlightEstimate("tzn"))
+        }
+        if (display.australia) {
+            newArray.push(getFlightEstimate("syd"))
+        }
+        if (display.americas) {
+            newArray.push(getFlightEstimate("ljn"))
+        }
+        if (display.asia) {
+            newArray.push(getFlightEstimate("pek"))
+        }
+        console.log(newArray)
+        Promise.all(newArray)
+            .then(arr => {
+                console.log(arr)
+                const total = arr.reduce((acc, current) => acc+current.data.attributes.carbon_mt, 0);
+                console.log(total);
                 });
         } 
         
-        const newEvent = {
-            target: {
-                name: "flight",
-                value: total
-            }
-        };
-        updateFormData(newEvent);
+        // const newEvent = {
+        //     target: {
+        //         name: "flight",
+        //         value: total
+        //     }
+        // };
+        // updateFormData(newEvent);
         
-    }
-
+    // }
+        
     return (
         <>
         <h2>Flights</h2>
@@ -60,7 +86,7 @@ const Flight = ({updateFormData}) => {
                     {display.europe &&
                     <>
                     <label htmlFor="flight amount" >Number of flights</label>
-                    <input type="number" id="number_of_flights_europe"/></>}
+                    <input onChange={onNumFlightChange} name="europe" type="number" id="number_of_flights_europe"/></>}
                     
                 </div>
                 <div>
@@ -70,7 +96,7 @@ const Flight = ({updateFormData}) => {
                     {display.africa &&
                     <>
                     <label htmlFor="flight amount" >Number of flights</label>
-                    <input type="number" id="number_of_flights_africa"/></>}
+                    <input onChange={onNumFlightChange} name="africa" type="number" id="number_of_flights_africa"/></>}
                 </div>
                 <div>
                     <input onChange={onChange} type="checkbox" id="australia" name="flights" value="syd"
@@ -79,7 +105,7 @@ const Flight = ({updateFormData}) => {
                     {display.australia &&
                     <>
                     <label htmlFor="flight amount" >Number of flights</label>
-                    <input type="number" id="number_of_flights_australia"/></>}
+                    <input onChange={onNumFlightChange} name="australia" type="number" id="number_of_flights_australia"/></>}
                 </div>
                 <div>
                     <input onChange={onChange} type="checkbox" id="americas" name="flights" value="ljn"
@@ -88,7 +114,7 @@ const Flight = ({updateFormData}) => {
                     {display.americas && 
                     <>
                     <label htmlFor="flight amount" >Number of flights</label>
-                    <input type="number" id="number_of_flights_americas"/></>}
+                    <input onChange={onNumFlightChange} name="americas" type="number" id="number_of_flights_americas"/></>}
                 </div>
                 <div>
                     <input onChange={onChange} type="checkbox" id="asia" name="flights" value="pek"/>
@@ -96,7 +122,7 @@ const Flight = ({updateFormData}) => {
                     {display.asia &&
                     <>
                     <label htmlFor="flight amount" >Number of flights</label>
-                    <input type="number" id="number_of_flights_asia"/></>}
+                    <input onChange={onNumFlightChange} name="asia" type="number" id="number_of_flights_asia"/></>}
                 </div>
                 <input type="submit" onClick={handleFlightClick} value="Submit Flights" id="Submit-Flights"/>
                 </>
