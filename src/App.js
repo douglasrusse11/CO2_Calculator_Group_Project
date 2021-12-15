38069be24e07e5e69b8718d860249bd585c066f9
David McIntyre <david.v.mcintyre@gmail.com>
2021-12-15 14:32:05 +0000

    cleaning app.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ import CarbonContainer from './container/CarbonContainer';
 function App() {
   
 
-
-
- 
-  //   .then((res) => res.json())
-  //   .then((res) => {
-  //     console.log(res);
-  //     setData(res);
-  //   })
-  //   .catch((error) => console.log(error))
-  // }
-
   return (
     <div className="App">
       <CarbonContainer />
