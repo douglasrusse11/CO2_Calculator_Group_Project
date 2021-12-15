4e460923bf3195728559f460e097d28ceab9c6bd
David McIntyre <david.v.mcintyre@gmail.com>
2021-12-15 14:29:30 +0000

    cleaned FinalCharts

--- src/components/FinalCharts.js
+++ src/components/FinalCharts.js
@@ -1,64 +1,3 @@
-// import Highcharts from 'highcharts'
-// import HighchartsReact from 'highcharts-react-official'
-
-// const FinalCharts = () => {
-
-//     const options = {
-
-//         title: {
-//             text: 'Average Carbon Emissions'
-//         },
-
-//         chart: {
-//             type: 'bar',
-//             height: 200,
-//             showAxes: false,
-//             backgroundColor: 'rgba(0,0,0,0)'
-//         },
-
-//         series: {
-//             data: [12.19, 6.76, 10.51],
-//         },
-
-//             xAxis: {
-//                 labels: {
-//                     enabled: false
-//                 },
-//                 title: {
-//                     text: null
-//                 },
-//                 startOnTick: false,
-//                 endOnTick: false,
-//                 tickPositions: [],
-//                 categories: ["Total Carbon Emissions"]
-//             },
-//             yAxis: {
-//                 endOnTick: false,
-//                 startOnTick: false,
-//                 labels: {
-//                     enabled: false
-//                 },
-//                 title: {
-//                     text: null
-//                 },
-//                 tickPositions: [0]
-//             },
-
-//             legend: {
-//                 reversed: true
-//             },
-
-//         }
-      
-//       return(
-//             <HighchartsReact highcharts={Highcharts} options={options}/>
-//         );
-      
-//     }
-
-// export default FinalCharts;
-
-
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import { useState, useEffect } from 'react';
@@ -110,7 +49,7 @@ const options = {
           
   plotOptions: {
     series: {
-    //   stacking: 'normal'
+
     }
   },
 
